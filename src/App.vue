fef75214451b2c91c0e904c1018a6358b6252e28
hemaddlawar <hema.200219627512@gmail.com>
2024-07-18 13:24:45 +0300

    add hero component and specify title by using props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavBar from "@/components/NavBar.vue";
+import Hero from "@/components/Hero.vue";
 </script>
 
 <template>
   <NavBar />
+  <Hero titlee="bzheepartee" />
 </template>
 
 <style scoped></style>
