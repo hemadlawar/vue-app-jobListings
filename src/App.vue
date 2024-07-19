fefea610571e80ada6940b824a5492ad758cd7ab
hemaddlawar <hema.200219627512@gmail.com>
2024-07-19 16:59:52 +0300

    add Home card and card

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import NavBar from "@/components/NavBar.vue";
 import Hero from "@/components/Hero.vue";
+import HomeCard from "@/components/HomeCard.vue";
 </script>
 
 <template>
   <NavBar />
   <Hero titlee="bzheepartee" />
+  <HomeCard />
 </template>
 
 <style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+<template>
+  <div class="bg-gray-100 p-6 rounded-lg shadow-md">
+    <h2 class="text-2xl font-bold">For Developers</h2>
+    <p class="mt-2 mb-4">Browse our Vue jobs and start your career today</p>
+    <a
+      href="jobs.html"
+      class="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+    >
+      Browse Jobs
+    </a>
+  </div>
+</template>
+<style scoped></style>
