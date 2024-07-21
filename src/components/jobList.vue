c83014752a111d0418ed2e6d0056798cbd00d151
hemaddlawar <hema.200219627512@gmail.com>
2024-07-21 16:56:03 +0300

    add truncate for the app

--- src/components/jobList.vue
+++ src/components/jobList.vue
@@ -1,31 +1,57 @@
 <script setup>
-import { defineProps } from "vue";
-const propss = defineProps({
+import { defineProps, ref, computed } from "vue";
+
+const props = defineProps({
   job: Object,
 });
 
-console.log(propss);
+const isDescriptionTruncated = ref(true);
+
+// Computed property to truncate the job description
+const displayedDescription = computed(() => {
+  const description = props.job.description;
+  const maxLength = 100; // Set your desired max length here
+  if (isDescriptionTruncated.value && description.length > maxLength) {
+    return description.substring(0, maxLength) + "...";
+  }
+  return description;
+});
+
+// Toggle function to show/hide the full description
+const toggleDescription = () => {
+  isDescriptionTruncated.value = !isDescriptionTruncated.value;
+};
+
+console.log(props);
 </script>
+
 <template>
   <div class="bg-white rounded-xl shadow-md relative">
     <div class="p-4">
       <div class="mb-6">
-        <div class="text-gray-600 my-2">{{ propss.job.type }}</div>
-        <h3 class="text-xl font-bold">{{ propss.job.title }}</h3>
+        <div class="text-gray-600 my-2">{{ props.job.type }}</div>
+        <h3 class="text-xl font-bold">{{ props.job.title }}</h3>
       </div>
 
       <div class="mb-5">
-        {{ propss.job.description }}
+        {{ displayedDescription }}
+        <button
+          v-if="props.job.description.length > 100"
+          @click="toggleDescription"
+          class="text-blue-500 hover:text-blue-700 ml-2"
+        >
+          {{ isDescriptionTruncated.value ? "Show Less" : "Read More" }}
+        </button>
       </div>
 
-      <h3 class="text-green-500 mb-2">{{ propss.job.salary }} / Year</h3>
+      <h3 class="text-green-500 mb-2">{{ props.job.salary }} / Year</h3>
 
       <div class="border border-gray-100 mb-5"></div>
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
           <i class="fa-solid fa-location-dot text-lg"></i>
-          {{ propss.job.location }}
+          {{ props.job.location }}
         </div>
         <a
           href="job.html"
@@ -37,4 +63,5 @@ console.log(propss);
     </div>
   </div>
 </template>
+
 <style></style>
