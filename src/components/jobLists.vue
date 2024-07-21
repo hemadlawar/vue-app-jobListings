2ab614038a642f80535c00b3c830978031981c58
hemaddlawar <hema.200219627512@gmail.com>
2024-07-21 15:40:19 +0300

    add job lists

--- src/components/jobLists.vue
+++ src/components/jobLists.vue
@@ -1,13 +1,17 @@
 <script setup>
 import arrayOFjson from "@/assets/jobs copy.json";
 import JobList from "@/components/jobList.vue";
-console.log(arrayOFjson);
 </script>
 <template>
-  <div v-for="job in arrayOFjson">
-    <div class="flex">
-      <JobList />
+  <section class="bg-green-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Job Browser
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobList v-for="job in arrayOFjson" />
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 <style></style>
